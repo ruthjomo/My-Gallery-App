6f5ac8895dcad0ecc6afa4f3831806f3c14f8d74
ruthjomo <ruthjomo19@gmail.com>
2020-10-11 20:30:51 +0300

    installed bootstrap

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'GalleryApp.apps.GalleryappConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
