83b6886b50aa0786cadc2f229ba044f0c456bd4d
ruthjomo <ruthjomo19@gmail.com>
2020-10-12 15:39:26 +0300

    added codes in files

--- GalleryApp/urls.py
+++ GalleryApp/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
+
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
@@ -7,4 +10,15 @@ urlpatterns=[
 urlpatterns=[
     #......
     url('^$',views.GalleryApp_of_day,name='GalleryAppToday')
+    url('^$',views.index,name = 'index'),
+    url(r'^search/', views.search_image, name='search_image'),
+    url(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.unique,name = 'unique')
+
 ]
+
+urlpatterns=[
+   
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- GalleryApp/views.py
+++ GalleryApp/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt
 from django.shortcuts import render
+from django.http  import HttpResponse, Http404
+from django.shortcuts import render, redirect
+from .models import Image, Location, Category
+
 
 # Create your views here.
 def welcome(request):
