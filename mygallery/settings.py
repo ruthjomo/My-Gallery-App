96d25dad906c725406dcf4b855d69febc351369d
ruthjomo <ruthjomo19@gmail.com>
2020-10-12 08:37:06 +0300

    added installations,new files and codes

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'mygallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mygallery',
+        'USER': '<>',
+    'PASSWORD':'<>',
     }
 }
 
